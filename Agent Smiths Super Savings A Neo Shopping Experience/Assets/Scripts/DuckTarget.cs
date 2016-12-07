06cc9eb7b6999575a546f36f60861b71cd111930
Justin Ryder <jryder@mywebgrocer.com>
2016-12-07 14:10:32 -0500

    Added end cap prefab. Added DuckTarget. Added DuckTarget cereal boxes to the end caps.

---
+++ Agent Smiths Super Savings A Neo Shopping Experience/Assets/Scripts/DuckTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckTarget : MonoBehaviour
+{
+    public Vector3 MoveDirection = Vector3.right;
+    public float MoveSpeed = 2;
+    private float _lifeTime = 0;
+    private Vector3 _startPosition;
+
+    void Start()
+    {
+        _startPosition = transform.position;
+        _lifeTime = Random.value;
+    }
+    
+    void Update()
+    {
+        _lifeTime += Time.deltaTime;
+        transform.position = _startPosition + transform.TransformVector(MoveDirection * Mathf.Sin(_lifeTime * MoveSpeed));
+    }
+}
